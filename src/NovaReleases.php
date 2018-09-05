3dac23fa5400e0e192f38052db87f638758e4681
Matt Stauffer <matt@tighten.co>
2018-09-05 12:55:22 -0400

    Initial commit

---
+++ src/NovaReleases.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tightenco\NovaReleases;
+
+use Laravel\Nova\Card;
+
+class NovaReleases extends Card
+{
+    /**
+     * The width of the card (1/3, 1/2, or full).
+     *
+     * @var string
+     */
+    public $width = '1/3';
+
+    /**
+     * Get the component name for the element.
+     *
+     * @return string
+     */
+    public function component()
+    {
+        return 'nova-releases';
+    }
+}
